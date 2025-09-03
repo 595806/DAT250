4084fb5d1210fd10da34e1c6f69f30e41ca0484c
595806 <90557386+595806@users.noreply.github.com>
2025-09-03 10:39:14 +0200

    init

---
+++ src/test/java/DAT250/Oblig/ObligApplicationTests.java
@@ -0,0 +1,13 @@
+package DAT250.Oblig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObligApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
